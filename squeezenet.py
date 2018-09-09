a492b1303e9b4f0b163546c076841dbed0da4167
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2018-09-09 15:20:32 +0800

    Refactor the fire_module and also add the dropoff layer in the right place.
    * TODO: Currently experimenting with the optimizer to more closely
    match the one in the paper.

--- squeezenet.py
+++ squeezenet.py
@@ -1,32 +1,29 @@
 from tensorflow import keras
 
 
-def fire_module(x, nb_filters, name):
-    (squeeze_nb_filters, expand_1x1_nb_filters, expand_3x3_nb_filters) = nb_filters
-
-    squeeze = keras.layers.Conv2D(filters=squeeze_nb_filters,
+def fire_module(x, nb_squeeze_filters, nb_expand_filters, name):
+    squeeze = keras.layers.Conv2D(filters=nb_squeeze_filters,
                                   kernel_size=1,
                                   padding='same',
                                   activation=keras.activations.relu,
                                   name=name + '/squeeze')(x)
 
-    expand_1x1 = keras.layers.Conv2D(filters=expand_1x1_nb_filters,
+    expand_1x1 = keras.layers.Conv2D(filters=nb_expand_filters,
                                      kernel_size=1,
                                      padding='same',
                                      activation=keras.activations.relu,
-                                     name=name + '/expand_1_1')(squeeze)
+                                     name=name + '/e1x1')(squeeze)
 
-    expand_3x3 = keras.layers.Conv2D(filters=expand_3x3_nb_filters,
+    expand_3x3 = keras.layers.Conv2D(filters=nb_expand_filters,
                                      kernel_size=3,
                                      padding='same',
                                      activation=keras.activations.relu,
-                                     name=name + '/expand_3_3')(squeeze)
+                                     name=name + '/e3x3')(squeeze)
 
     return keras.layers.concatenate([expand_1x1, expand_3x3], axis=3, name=name + '/concat')
 
 
 def SqueezeNet(input_shape=None, classes=None):
-
     input_tensor = keras.layers.Input(shape=input_shape)
 
     x = keras.layers.Conv2D(filters=96,
@@ -40,34 +37,34 @@ def SqueezeNet(input_shape=None, classes=None):
                                   padding='same',
                                   name='maxpool1')(x)
 
-    x = fire_module(x, (16, 64, 64), name='fire2')
-    x = fire_module(x, (16, 64, 64), name='fire3')
-    x = fire_module(x, (32, 128, 128), name='fire4')
+    x = fire_module(x, 16, 64, name='fire2')
+    x = fire_module(x, 16, 64, name='fire3')
+    x = fire_module(x, 32, 128, name='fire4')
 
     x = keras.layers.MaxPooling2D(pool_size=3,
                                   strides=2,
                                   padding='same',
                                   name='maxpool4')(x)
 
-    x = fire_module(x, (32, 128, 128), name='fire5')
-    x = fire_module(x, (48, 192, 192), name='fire6')
-    x = fire_module(x, (48, 192, 192), name='fire7')
-    x = fire_module(x, (64, 256, 256), name='fire8')
+    x = fire_module(x, 32, 128, name='fire5')
+    x = fire_module(x, 48, 192, name='fire6')
+    x = fire_module(x, 48, 192, name='fire7')
+    x = fire_module(x, 64, 256, name='fire8')
 
     x = keras.layers.MaxPooling2D(pool_size=3,
                                   strides=2,
                                   padding='same',
                                   name='maxpool8')(x)
 
-    x = fire_module(x, (64, 256, 256), name='fire9')
+    x = fire_module(x, 64, 256, name='fire9')
+
+    x = keras.layers.Dropout(0.5)(x)
 
     x = keras.layers.Conv2D(filters=classes,
                             kernel_size=1,
                             strides=1,
                             padding='same',
                             name='conv10')(x)
-
-    x = keras.layers.Dropout(0.5)(x)
 
     x = keras.layers.AveragePooling2D(pool_size=13)(x)
     x = keras.layers.Flatten()(x)
--- train.py
+++ train.py
@@ -5,7 +5,6 @@ from tensorflow import keras
 import os
 import numpy as np
 
-tf.enable_eager_execution()
 tf.logging.set_verbosity(tf.logging.INFO)
 
 from squeezenet import SqueezeNet
@@ -34,19 +33,34 @@ test_images = np.dstack((test_images,) * 3)
 test_images = test_images.reshape(-1, 28, 28, 3)
 
 CLASSES = 10
-BATCH_SIZE = 256
-EPOCHS = 10
+BATCH_SIZE = 128
+EPOCHS = 15
 
-model = SqueezeNet(input_shape=(224, 224, 3), classes=CLASSES)
-model.compile(optimizer=tf.train.RMSPropOptimizer(0.001),
+decay_steps = len(train_images) * EPOCHS // BATCH_SIZE
 
+model = SqueezeNet(input_shape=(227, 227, 3), classes=CLASSES)
+
+# learning_rate = tf.train.polynomial_decay(learning_rate=0.04,
+#                                           global_step=tf.train.get_or_create_global_step(),
+#                                           decay_steps=decay_steps,
+#                                           end_learning_rate=0.005,
+#                                           power=1.0,
+#                                           cycle=False)
+#
+# optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate,
+#                                        momentum=0.9,
+#                                        use_nesterov=True)
+
+optimizer = tf.train.RMSPropOptimizer(learning_rate=0.001)
+
+model.compile(optimizer=optimizer,
               loss=keras.losses.categorical_crossentropy,
               metrics=['accuracy'])
 
 
 def input_fn(features, labels, batch_size, nb_classes, training=True):
     def _parse_function(feature, label):
-        feature = tf.image.resize_images(feature / 255, [224, 224])
+        feature = tf.image.resize_images(feature / 255, [227, 227])
 
         # NOTE: Must turn into one hot so that we get (10,) for labels
         return feature, tf.one_hot(label, nb_classes)
@@ -56,7 +70,7 @@ def input_fn(features, labels, batch_size, nb_classes, training=True):
     if training:
         dataset = dataset.shuffle(1000).repeat()
 
-    dataset = dataset.batch(batch_size, drop_remainder=True)\
+    dataset = dataset.batch(batch_size, drop_remainder=True) \
         .map(_parse_function, num_parallel_calls=multiprocessing.cpu_count())
 
     return dataset
@@ -65,15 +79,18 @@ def input_fn(features, labels, batch_size, nb_classes, training=True):
 print(model.summary())
 
 # NOTE: steps = (no of ex / batch_size) * no_of_epochs
-steps = int((len(train_labels) / BATCH_SIZE) * EPOCHS)
+steps = len(train_labels) // BATCH_SIZE * EPOCHS
 
 print('Number of steps {}'.format(steps))
 
-estimator = keras.estimator.model_to_estimator(keras_model=model)
+estimator = keras.estimator.model_to_estimator(keras_model=model,
+                                               model_dir=os.path.join(os.getcwd(), 'data', 'model'))
+
 estimator.train(input_fn=lambda: input_fn(train_images,
                                           train_labels,
                                           batch_size=BATCH_SIZE,
-                                          nb_classes=CLASSES), steps=steps)
+                                          nb_classes=CLASSES),
+                steps=steps)
 
 eval_result = estimator.evaluate(input_fn=lambda: input_fn(test_images,
                                                            test_labels,
